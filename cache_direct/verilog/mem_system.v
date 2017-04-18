4b321cb123c0c65e6fc0ead375d64ef7e391478e
zostale <shyamalanadkat@gmail.com>
2017-04-18 13:30:51 -0500

    cache states updated

--- cache_direct/verilog/mem_system.v
+++ cache_direct/verilog/mem_system.v
@@ -23,24 +23,36 @@ module mem_system(/*AUTOARG*/
    output CacheHit;
    output err;
 
+
+   wire [4:0] curr_state, next_state;
+   wire mem_err, cache_err, err, dirty, valid, stall;
+
    //**************STATES**************//
 
-   localparam IDLE 			= 4'b0000;
-   localparam COMP_RD		=
-   localparam COMP_WR		=
-   localparam WRITEBACK0	=	
-   localparam WRITEBACK1	=
-   localparam WRITEBACK2	=
-   localparam WRITEBACK3	=
-   localparam 
+   localparam IDLE 			= 5'h00;
+   localparam COMP			= 5'h01;
+   localparam WR			= 5'h02;
+   localparam WRITEBACK1	= 5'h03;
+   localparam WRITEBACK2	= 5'h04;
+   localparam WRITEBACK3	= 5'h05;
+   localparam WRITEBACK4	= 5'h06;
+   localparam READ1			= 5'h07;
+   localparam READ2			= 5'h08;
+   localparam READ3			= 5'h09;
+   localparam READ4			= 5'h0A;
+   localparam HIT 			= 5'h0B;
+   localparam MISS1			= 5'h0C;
+   localparam MISS2			= 5'h0D;
+   localparam MISS3			= 5'h0E;
+   localparam MISS4			= 5'h0F; 
+   localparam DONE			= 5'h10;
 
-
-
+  //**************STATES**************//
 
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
    parameter mem_type = 0;
-   cache #(0 + memtype) c0(// Outputs
+   cache #(0 + mem_type) c0(// Outputs
                           .tag_out              (),
                           .data_out             (),
                           .hit                  (),
@@ -76,6 +88,38 @@ module mem_system(/*AUTOARG*/
 
    
    // your code here
+
+   //assign err = (mem_err | cache_err);
+
+
+   dff dffmod [4:0] (.q(curr_state), .d(next_state), .clk(clk), .rst(rst));
+
+   always @(*) begin
+   	casex(state, Rd, Wr, hit, valid, dirty, stall)
+
+   		11'b0000000xxxx: begin
+   		assign next_state = IDLE;
+   		end
+
+   		11'b00000x1xxxx: begin
+   		assign next_state = COMP;
+   		end
+
+   		11'b000001xxxxx: begin
+   		assign next_state = COMP;
+   		end
+
+
+
+
+
+
+
+
+   	endcase
+   end
+
+
 
    
 endmodule // mem_system

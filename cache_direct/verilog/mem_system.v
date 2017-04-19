d3b33a83c31ed40d8cb2965a3f15fc8cd7a51af7
zostale <shyamalanadkat@gmail.com>
2017-04-18 23:05:18 -0500

    update on direct

--- cache_direct/verilog/mem_system.v
+++ cache_direct/verilog/mem_system.v
@@ -38,7 +38,7 @@ module mem_system(/*AUTOARG*/
    wire [7:0] index; 
    reg enable;
    reg comp; 
-   wire cache_write;
+   reg cache_write;
    wire [4:0] tag_in;
    wire [15:0] cache_data_in; 
    wire valid_in; 
@@ -49,6 +49,7 @@ module mem_system(/*AUTOARG*/
    wire cache_err, valid, dirty, hit; 
    wire [4:0] tag_out; 
    wire [15:0] cache_data_out; 
+   reg Done; 
 
 
 
@@ -58,12 +59,15 @@ module mem_system(/*AUTOARG*/
    wire mem_err;
    wire[15:0] mem_data_out;
 
+   //INPUT TO MEM_BANK
+   reg wr, rd; 
+
 
    //**************STATES**************//
 
-   localparam IDLE 			= 5'h00;
-   localparam COMPRD		= 5'h01;
-   localparam COMPWR		= 5'h02;
+   localparam IDLE 			  = 5'h00;
+   localparam COMPRD		  = 5'h01;
+   localparam COMPWR		  = 5'h02;
    localparam WRITEBACK0	= 5'h03;
    localparam WRITEBACK1	= 5'h04;
    localparam WRITEBACK2	= 5'h05;
@@ -76,7 +80,8 @@ module mem_system(/*AUTOARG*/
    localparam STORECACHE1	= 5'h0C;
    localparam STORECACHE2	= 5'h0D;
    localparam STORECACHE3	= 5'h0E; 
-   localparam DONE			= 5'h0F;
+   localparam DONE			  = 5'h0F;
+   localparam ERR         = 5'h10;
 
   //*********************************//
 
@@ -125,79 +130,165 @@ module mem_system(/*AUTOARG*/
   assign offset = Addr[2:0];      //if lsb is 1 then err, o.w 0
 
 
-   dff dffmod [4:0] (.q(curr_state), .d(next_state), .clk(clk), .rst(rst));
-
+  dff dffmod [4:0] (.q(curr_state), .d(next_state), .clk(clk), .rst(rst));
 
    always@(*)begin 
+   comp = 1'b0;
+   Done = (hit & valid); 
+
     case(curr_state)
 
     	IDLE: begin
 
-
+      wr = 0;
+      rd = 0; 
+      cache_write = 1'b0;
+      Done = (Wr | Rd) & (hit & valid);
+      next_state = ((Rd & Wr) | mem_err) ? ERR: (Rd & ~Wr) ? COMPRD : (~Rd & Wr) ? COMPWR : (~Rd & ~Wr) ? IDLE : ERR;
     	end
 
     	COMPRD: begin
 
+      comp = 1'b1; 
+      cache_write = 1'b0;
 
     	end
 
     	COMPWR: begin
 
+      comp = 1'b1; 
+      cache_write = 1'b1;
+
+      next_state = (valid & hit) ? IDLE : (~valid & ~hit & dirty) ? WRITEBACK0 : (~valid & ~hit & ~dirty) ? MEMREAD0 : ERR; 
     	end
 
     	WRITEBACK0: begin
+      comp = 1'b0;
+      cache_write = 1'b0;
+      wr = 1'b1; 
 
+      next_state = (busy[0]) ? WRITEBACK0 : WRITEBACK1;
     	end
 
     	WRITEBACK1: begin
+      comp = 1'b0;
+      cache_write = 1'b0;
+      wr = 1'b1; 
 
+
+      next_state = (busy[1]) ? WRITEBACK1: WRITEBACK2;
     	end
 
     	WRITEBACK2: begin
+      comp = 1'b0;
+      cache_write = 1'b0;
+      wr = 1'b1; 
 
+      next_state = (busy[2]) ? WRITEBACK2: WRITEBACK3;
     	end
 
 
     	WRITEBACK3: begin
 
+      comp = 1'b0;
+      cache_write = 1'b0;
+      wr = 1'b1; 
+
+      next_state = (busy[3]) ? WRITEBACK3: MEMREAD0;
+
     	end
 
     	MEMREAD0: begin
+
+      comp = 1'b0;
+      cache_write = 1'b0;
+      rd = 1'b1; 
+      enable = 1'b0;
+
+      next_state = (busy[0]) ? MEMREAD0: MEMREAD1;
 
     	end
 
     	MEMREAD1: begin
 
+      comp = 1'b0;
+      cache_write = 1'b0;
+      rd = 1'b1;
+      enable = 1'b0;
+
+      next_state = (busy[1]) ? MEMREAD1: MEMREAD2;
+
     	end
 
     	MEMREAD2: begin
 
+      comp = 1'b0;
+      cache_write = 1'b0;
+      rd = 1'b1;
+      enable = 1'b0;
+
+      next_state = (busy[2]) ? MEMREAD2: MEMREAD3;
 
     	end
 
     	MEMREAD3: begin
 
+      comp = 1'b0;
+      cache_write = 1'b0;
+      rd = 1'b1;
+      enable = 1'b0;
+
+      next_state = (busy[3]) ? MEMREAD3: STORECACHE0;
 
     	end
 
 
     	STORECACHE0: begin
 
+      comp = 1'b0;
+      cache_write = 1'b1;
+      enable  = 1'b1;
+
+
+      next_state = STORECACHE1;
     	end
 
 
     	STORECACHE1: begin
+
+      comp = 1'b0;
+      cache_write = 1'b1;
+      enable  = 1'b1;
+
+      next_state = STORECACHE2;
 
     	end
 
 
     	STORECACHE2: begin
 
+      comp = 1'b0;
+      cache_write = 1'b1;
+      enable  = 1'b1;
+
+      next_state = STORECACHE3; 
     	end
 
     	STORECACHE3: begin
 
+      comp = 1'b0;
+      cache_write = 1'b1;
+      enable  = 1'b1;
+
+      next_state = (mem_stall) ? STORECACHE3: IDLE;
+
     	end
+
+      ERR: begin
+
+      next_state = (~mem_err) ? IDLE : ERR;
+      Done = 1'b1;
+      end
 
 
  		DONE: begin
